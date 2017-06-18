767ae0774565569ad581a99ff91dc23d43cb0a98
hphilamore <hemma.philamore@bristol.ac.uk>
2017-06-18 16:32:57 +0900

    direction switching commented out

--- L298_dual_motor_controller.ino
+++ L298_dual_motor_controller.ino
@@ -1,12 +1,12 @@
 // connect motor controller pins to Arduino digital pins
 // motor one
-int enA = 10;
-int in1 = 9;
-int in2 = 8;
+int enA = 5;
+int in1 = 7;
+int in2 = 6;
 // motor two
-int enB = 5;
-int in3 = 7;
-int in4 = 6;
+int enB = 8;
+int in3 = 10;
+int in4 = 9;
 
 void setup()
 {
@@ -34,16 +34,16 @@ digitalWrite(in4, LOW);
 analogWrite(enB, 250);
 delay(2000);
 // now change motor directions
-digitalWrite(in1, LOW);
-digitalWrite(in2, HIGH);
-digitalWrite(in3, LOW);
-digitalWrite(in4, HIGH);
-delay(2000);
-// now turn off motors
-digitalWrite(in1, LOW);
-digitalWrite(in2, LOW);
-digitalWrite(in3, LOW);
-digitalWrite(in4, LOW);
+//digitalWrite(in1, LOW);
+//digitalWrite(in2, HIGH);
+//digitalWrite(in3, LOW);
+//digitalWrite(in4, HIGH);
+//delay(2000);
+//// now turn off motors
+//digitalWrite(in1, LOW);
+//digitalWrite(in2, LOW);
+//digitalWrite(in3, LOW);
+//digitalWrite(in4, LOW);
 }
 
 void demoTwo()
